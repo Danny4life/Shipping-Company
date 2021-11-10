fbf4efc5e7cc8dd174d8c797b90a2c629de22976
segun <osiki.segun@gmail.com>
2021-11-10 16:24:04 +0100

    adding files

---
+++ script.js
@@ -0,0 +1,13 @@
+const btnOne = document.querySelector("#button-1");
+
+btnOne.addEventListener("click", () => {
+     
+    location.href = "file:///home/segun/Shipping-Company/TrackNow.html";
+});
+
+const btnTwo = document.querySelector("#button-2");
+
+btnTwo.addEventListener("click", () => {
+
+    location.href = "file:///home/segun/Shipping-Company/Contact.html";
+});
